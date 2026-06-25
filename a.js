17492a205b4988d406db67b57152d8cc6ac09e28
muskansalaria966 <muskansalaria966@gmail.com>
2026-06-25 19:51:27 +0530

    h

---
+++ a.js
@@ -0,0 +1,282 @@
+// console.log("Muskan")
+// alert("welcome");
+// let name=prompt("Enter name")
+// console.log(name)
+// let name=prompt("Enter name")
+// alert("welcome to world" + name)
+
+// let name="muskan"
+// console.log(typeof name)
+
+// let user
+// console.log(typeof user)
+
+// let number=2
+// console.log(typeof number)
+
+// let null= null
+// console.log(typeof null)
+
+// let bool=true
+// console.log(typeof bool)
+
+// let n=BigInt("37575465436578468747464778346874674687467868746874468768763872487673523");
+// console.log(typeof n)
+
+//var support reassign and redeclare
+// var name="muskan"
+// name="salaria"
+// console.log(name)
+
+//let only support reassign otherwise it shows syntax error
+// let name="muskan"
+// name="salaria"
+// console.log(name)
+
+//this doesnot allow reassign and redeclare
+
+// const name="muskan"
+// name="salaria"
+// console.log(name)
+
+// function great(){
+//     console.log("hello")
+// }
+// great()
+
+// function great(name){
+//     console.log("hello " + name)
+// }
+// great("muskan")
+// great("salariaa")
+// great("hgh")
+
+// function sum(a,b,c){
+//     console.log(a+b+c)
+// }
+// sum(1,2,3)
+
+// function sub(a,b,c){
+//     console.log(a-b-c)
+// }
+// sub(1,2,3)
+
+// function mul(a,b,c){
+//     console.log(a*b*c)
+// }
+// mul(1,2,3)
+
+// function div(a,b,c){
+//     console.log(a/b/c)
+// }
+// div(1,2,3)
+//  function mod(a,b,c){
+//     console.log(a%b%c)
+// }
+// mod(1,2,3)
+ //Arithmetic operators
+// let a=10;
+// let b=20;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a**b);
+// console.log(a/b);
+// console.log(a%b);
+// console.log(a++);
+// console.log(a--);
+// console.log(b--);
+// console.log(b++);
+// console.log();
+
+//assignment operators
+
+// let x=20;
+// let y=10;
+
+// console.log(x=y);
+// console.log(x+=y);
+// console.log(x-=y);
+// console.log(x*=y);
+// console.log(x/=y);
+// console.log(x%=y);
+// console.log(x**=y);
+
+//Comparison operators
+
+// let x=10;
+// let n=10;
+
+// console.log(x==n);
+// console.log(x===n);
+// console.log(x!=n);
+// console.log(x!==n);
+// console.log(x>n);
+// console.log(x<n);
+// console.log(x>=n);
+// console.log(x<=n);
+
+//Logical operator
+
+// let x=18;
+// let n= true;
+
+// if(x>=18 && n){
+//     console.log("you can drive");
+// }
+
+// let hasID=true;
+// let User=false;
+
+// if(a || b){
+//     console.log("pass");
+// }
+
+// let user=false;
+
+// if(!user){
+//     console.log("access");
+// }
+
+
+// let marks=10;
+// let attendance=8;
+
+// if(!marks){
+//     console.log("pass");
+// }else{
+//     console.log("fail");
+// }
+
+// let age=19;
+// let eligibility=true;
+
+// if (age>=18 && eligibility){
+//     console.log("can vote");
+// }else{
+//     console.log("can't vote")
+// }
+
+// let marks=98;
+// let promote=true;
+
+// if(marks>=99 && promote){
+//     console.log("Promote");
+// }else{
+//     console.log("Not Promoted");
+// }
+
+// for(let i=1;i<=10;i++){
+//     console.log(i);
+// }
+
+// let i=1;
+// while(i<=10){
+//     console.log(i);
+//     i++;
+// }
+
+
+// let i=1;
+// do{
+//     console.log(i);
+//     i++;
+// }while(i<=10)
+
+// let n=1000;
+// for(let i=1;i<=n;i++){
+//     if(i%2===0){
+//         console.log(i);
+//     }
+// }
+
+// for(let i=1;i<=4;i++){
+//     for(let j=1;j<=i;j++){
+//     console.log("*");
+//     }
+//     console.log("\n");
+// }
+
+// for(let i=1;i<=5;i++){
+//     let row="";
+//     for(let j=1;j<=i;j++){
+//         row+='*';
+//     }
+//     console.log(row);
+// }
+
+// for(let i=5;i>=1;i--){
+//     let row="";{
+//         for(let j=1;j<=i;j++){
+//             row+='*';
+//         }
+//         console.log(row);
+//     }
+// }
+// const N=10;
+// for(let i=1;i<=N;i++){
+//     let row="";{
+//         for(let j=1;j<=N-i+1;j++){
+//             row+='*';
+//         }
+//         console.log(row);
+//     }
+// }
+//pyramid
+//  const N=10;
+//  for(let i=1;i<=N;i++){
+//     let row="";{
+    
+//         for(let j=1;j<=N-i;j++){
+//              row+= " ";}
+//             for(let j=1;j<=2*i-1;j++){
+//                 row+='*';
+//             }
+        
+//         console.log(row);
+//     }
+// }
+
+// let add=(a,b)=>(a+b)
+// console.log(add(2,3))
+
+// function Fruits(items){
+//     items.forEach(items =>{
+//         console.log(items);}) ;
+// }
+
+// Fruits  (['apple','banana','mango','pineapple']);
+
+
+// function marks(score){
+//     score.forEach(score=>{
+//         console.log(score*2);
+//     }
+
+//     );
+// }
+
+// marks([12,23,45,67]);
+
+// let num=[2,3,34,6,7,5,8];
+
+// let even=num.filter(num=>num%2===0)
+// {
+//     console.log(even);
+// }
+
+// let Vegetables=['potato','Tomato','Cucumber'];
+// console.log(Vegetables.toString());
+// console.log(Vegetables.pop());
+// console.log(Vegetables);
+
+// Vegetables.push('lemon');
+// console.log(Vegetables);    
+
+let Fruits=['Apple','Banana','Pineapple','Mango'];
+// console.log(Fruits.toString());
+// console.log(Fruits.join(""));
+// console.log(Fruits.pop());
+// console.log(Fruits);
+
+
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+
+// let num=[2,3,34,6,7,5,8];
+
+// let even=num.filter(num=>num%2===0)
+// {
+//     console.log(even);
+// }
